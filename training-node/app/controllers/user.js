0a7add7616697dc1497184648ca505e010522373
mpicollo-wolox <martin.picollo@wolox.com.ar>
2018-03-06 14:35:29 -0300

    Add object instead of promise for validate user + Make new object for modify password

--- training-node/app/controllers/user.js
+++ training-node/app/controllers/user.js
@@ -3,38 +3,53 @@ const userServices = require('../services/user'),
   errors = require('../errors'),
   bcrypt = require('bcrypt');
 
-const validateUser = user =>
-  new Promise((resolve, reject) => {
-    if (
-      user.password &&
-      (user.password.length < 8 || !user.password.match('([A-Za-z]+[0-9]+)|([0-9]+[A-Za-z]+)'))
-    ) {
-      reject(errors.savingError('Password of user must be alphanumeric and 8 characters minimum'));
-    }
+const validateUser = user => {
+  const validation = {
+    message: '',
+    isValid: true
+  };
+  if (
+    user.password &&
+    (user.password.length < 8 || !user.password.match('([A-Za-z]+[0-9]+)|([0-9]+[A-Za-z]+)'))
+  ) {
+    validation.isValid = false;
+    validation.message = 'Password of user must be alphanumeric and 8 characters minimum';
+    return validation;
+  }
 
-    if (user.email && !user.email.match('^[A-Za-z0-9._%+-]+@wolox.com.ar')) {
-      reject(errors.savingError('Email invalid'));
-    }
+  if (user.email && !user.email.match('^[A-Za-z0-9._%+-]+@wolox.com.ar')) {
+    validation.isValid = false;
+    validation.message = 'Email invalid';
+    return validation;
+  }
 
-    resolve(user);
-  });
+  return validation;
+};
 
 exports.create = (request, response, next) => {
   const SALT_ROUNDS = 10;
   bcrypt
     .hash(request.user.password, SALT_ROUNDS)
     .then(hashPass => {
-      request.user.password = hashPass;
-      validateUser(request.user)
-        .then(userServices.create)
-        .then(userSaved => {
-          logger.info(`New user created with email [${userSaved.email}]`);
-          response.status(200).end();
-        })
-        .catch(err => {
-          logger.error(`Error creating user with email [${request.user.email}]`);
-          next(err);
-        });
+      const validation = validateUser(request.user);
+
+      if (validation.isValid) {
+        const userHash = request.user;
+        userHash.password = hashPass;
+
+        userServices
+          .create(userHash)
+          .then(userSaved => {
+            logger.info(`New user created with email [${userSaved.email}]`);
+            response.status(200).end();
+          })
+          .catch(err => {
+            logger.error(`Error creating user with email [${request.user.email}]`);
+            next(err);
+          });
+      } else {
+        next(errors.savingError(validation.message));
+      }
     })
     .catch(error => errors.defaultError(error));
 };
